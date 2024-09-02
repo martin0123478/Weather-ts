390cf3bda3319659eeff5e7ea5e1b67f6cdba775
martin0123478 <martin_agui97@outlook.es>
2024-09-02 13:44:57 -0600

    feat:add city and country to state

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,31 @@
+
+import type { SearchTypes } from "../../types"
+import { useState } from "react"
 import { countries } from "../../data/countries"
 import styles from './Form.module.css'
 export const Form = () => {
+    const [search, setSearch] = useState<SearchTypes>({
+        city: '',
+        country: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
     return (
         <form className={styles.form}>
             <div className={styles.field}>
                 <label htmlFor="city">Ciudad</label>
-                <input id="city" type="text" name="city" placeholder="Ciudad" />
+                <input id="city" type="text" name="city" placeholder="Ciudad"
+                    value={search.city}
+                    onChange={handleChange} />
             </div>
             <div className={styles.field}>
                 <label htmlFor="country">Pais</label>
-                <select name="" id="">
+                <select value={search.country} onChange={handleChange} name="country" id="country">
                     <option value="">--Seleccione un País--</option>
                     {countries.map(country => (
                         <option
